d519f9180084dd5b49e04e627fa6932ce7cf2602
your_github_username <valentinaleonh8@gmail.com>
2024-06-18 21:07:15 -0600

    tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @services = Service.all
+    @services = Service.all.sample(3)
   end
 end
