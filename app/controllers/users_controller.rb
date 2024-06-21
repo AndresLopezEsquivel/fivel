6f60cf28d415d391599a7b5f668a4f4dcf14e6b4
Donovan Montiel <dmontieloliveros@gmail.com>
2024-06-20 19:40:48 -0600

    Avance al profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user_language, only: [:edit_language, :update_language, :destroy_language]
+  before_action :set_user_skill, only: [:edit_skill, :update_skill, :destroy_skill]
+  before_action :set_user_education, only: [:edit_education, :update_education, :destroy_education]
+  before_action :set_user_certification, only: [:edit_certification, :update_certification, :destroy_certification]
 
   def profile
     @user_languages = UserLanguage.where(user: current_user)
@@ -42,14 +45,6 @@ class UsersController < ApplicationController
     @user_skill = UserSkill.new
   end
 
-  def new_education
-    @user_education = UserEducation.new
-  end
-
-  def new_certification
-    @user_certification = UserCertification.new
-  end
-
   def create_skill
     @user_skill = UserSkill.new(skill_params)
     @user_skill.user = current_user
@@ -58,6 +53,26 @@ class UsersController < ApplicationController
     else
       render :new_skill, status: :unprocessable_entity
     end
+  end
+
+  def edit_skill
+  end
+
+  def update_skill
+    if @user_skill.update(skill_params)
+      redirect_to profile_path, notice: 'Skill was successfully updated.'
+    else
+      render :edit_skill
+    end
+  end
+
+  def destroy_skill
+    @user_skill.destroy
+    redirect_to profile_path, notice: 'Skill was successfully deleted.'
+  end
+
+  def new_education
+    @user_education = UserEducation.new
   end
 
   def create_education
@@ -70,6 +85,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit_education
+  end
+
+  def update_education
+    if @user_education.update(education_params)
+      redirect_to profile_path, notice: 'Education was successfully updated.'
+    else
+      render :edit_education
+    end
+  end
+
+  def destroy_education
+    @user_education.destroy
+    redirect_to profile_path, notice: 'Education was successfully deleted.'
+  end
+
+  def new_certification
+    @user_certification = UserCertification.new
+  end
+
   def create_certification
     @user_certification = UserCertification.new(certification_params)
     @user_certification.user = current_user
@@ -78,6 +113,22 @@ class UsersController < ApplicationController
     else
       render :new_certification, status: :unprocessable_entity
     end
+  end
+
+  def edit_certification
+  end
+
+  def update_certification
+    if @user_certification.update(certification_params)
+      redirect_to profile_path, notice: 'Certification was successfully updated.'
+    else
+      render :edit_certification
+    end
+  end
+
+  def destroy_certification
+    @user_certification.destroy
+    redirect_to profile_path, notice: 'Certification was successfully deleted.'
   end
 
   private
@@ -90,12 +141,24 @@ class UsersController < ApplicationController
     params.require(:user_language).permit(:language, :level)
   end
 
+  def set_user_skill
+    @user_skill = UserSkill.find(params[:skill_id])
+  end
+
   def skill_params
     params.require(:user_skill).permit(:skill, :level)
   end
 
+  def set_user_education
+    @user_education = UserEducation.find(params[:education_id])
+  end
+
   def education_params
     params.require(:user_education).permit(:country, :college, :title, :major, :year)
+  end
+
+  def set_user_certification
+    @user_certification = UserCertification.find(params[:certification_id])
   end
 
   def certification_params
